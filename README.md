374e8ceabae163465d2ec61de7e61b7fbadbe735
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-01-16 13:45:21 +0100

    first version

---
+++ README.md
@@ -0,0 +1,60 @@
+# Joplin note overview
+
+A note overview is created based on the defined search and the specified fields.
+
+<img src=img/main.jpg>
+
+## Installation
+
+### Automatic
+
+- Go to `Tools > Options > Plugins`
+- Search for `combine-notes`
+- Click Install plugin
+- Restart Joplin to enable the plugin
+
+### Manual
+
+- Download the latest released JPL package (`io.github.jackgruber.note-overview.jpl`) from [here](https://github.com/JackGruber/joplin-plugin-note-overview/releases/latest)
+- Close Joplin
+- Copy the downloaded JPL package in your profile `plugins` folder
+- Start Joplin
+
+## Usage
+
+Create one or more notes with the following content:
+
+```md
+<!-- note-overview-plugin
+search: -tag:*
+fields: updated_time, title
+sort: title DESC
+-->
+```
+
+- `search` Search filters like in Joplin [Documentation of search filters](https://joplinapp.org/#search-filters)
+- `fields` Which fields should be output in the table. [Documentation of the possible fields](https://joplinapp.org/api/references/rest_api/#properties)
+- `sort` By which field should be sorted. \<field\> DESC/ASC
+
+The note content is updated every 5 minutes or manualy by `Tools > Create note overview`.
+
+## Build
+
+To build your one version of the plugin, install node.js and run the following command `npm run dist`
+
+## Updating the plugin framework
+
+To update the plugin framework, run `npm run update`
+
+## Changelog
+
+### v1.0.0 (2021-01-16)
+
+- First version
+
+## Links
+
+- [Joplin - Getting started with plugin development](https://joplinapp.org/api/get_started/plugins/)
+- [Joplin - Plugin API reference](https://joplinapp.org/api/references/plugin_api/classes/joplin.html)
+- [Joplin - Data API reference](https://joplinapp.org/api/references/rest_api/)
+- [Joplin - Plugin examples](https://github.com/laurent22/joplin/tree/dev/packages/app-cli/tests/support/plugins)
---
+++ src/index.ts
@@ -0,0 +1,158 @@
+import joplin from "api";
+import { MenuItemLocation } from "api/types";
+
+joplin.plugins.register({
+  onStart: async function () {
+    console.info("Note overview plugin started!");
+
+    await joplin.commands.register({
+      name: "createNoteOverview",
+      label: "Create note overview",
+      execute: async () => {
+        runCreateNoteOverview();
+      }
+    });
+
+    await joplin.views.menuItems.create(
+      "menuItemToolsCreateNoteOverview",
+      "createNoteOverview",
+      MenuItemLocation.Tools
+    );
+
+    async function runCreateNoteOverview() {
+      // search all notes
+      let pageNum = 1;
+      let overviewNotes = null;
+      let queryNotes = null;
+      const now = new Date();
+      do {
+        overviewNotes = await joplin.data.get(["search"], {
+          query: '/"<!-- note-overview-plugin"',
+          fields: "id, title, body",
+          limit: 10,
+          page: pageNum++,
+        });
+
+        for (let overviewNotesKey in overviewNotes.items) {
+          let noteBody = overviewNotes.items[overviewNotesKey].body;
+          let noteId = overviewNotes.items[overviewNotesKey].id;
+          let noteTitle = overviewNotes.items[overviewNotesKey].title;
+          let settingsBlock = noteBody.match(/^(<!--(?:.|\n)*?-->)/);
+          if (settingsBlock) {
+            console.info("Check note " + noteTitle + " (" + noteId + ")");
+            settingsBlock = settingsBlock[1];
+            let query: string = await getParameter(
+              settingsBlock,
+              "search",
+              null
+            );
+            let fields: string = await getParameter(
+              settingsBlock,
+              "fields",
+              null
+            );
+            let sort: string = await getParameter(settingsBlock, "sort", "title ASC");
+            if (query) {
+              let fieldsArray = [];
+              if (fields) {
+                fieldsArray = fields
+                  .toLowerCase()
+                  .replace(/\s/g, "")
+                  .split(",");
+              } else {
+                fieldsArray = ["update_time", "title"];
+              }
+              let sortArray = sort.toLowerCase().split(" ");
+              let newBody = [];
+              newBody.push("| " + fieldsArray.join(" | ") + " |");
+              newBody.push("|" + " --- |".repeat(fieldsArray.length));
+
+              if(!sortArray[1]){
+                sortArray[1] = "ASC"
+              }
+              let pageQueryNotes = 1;
+              do {
+                queryNotes = await joplin.data.get(["search"], {
+                  query: query,
+                  fields: "id," + fieldsArray.join(","),
+                  order_by: sortArray[0],
+                  order_dir: sortArray[1].toUpperCase(),
+                  limit: 50,
+                  page: pageQueryNotes++,
+                });
+                for (let queryNotesKey in queryNotes.items) {
+                  if (queryNotes.items[queryNotesKey].id != noteId) {
+                    let noteInfos = [];
+                    for (let field in fieldsArray) {
+                      if (fieldsArray[field] === "title") {
+                        noteInfos.push(
+                          "[" +
+                            queryNotes.items[queryNotesKey].title +
+                            "](:/" +
+                            queryNotes.items[queryNotesKey].id +
+                            ")"
+                        );
+                      } else if (
+                        [
+                          "created_time",
+                          "updated_time",
+                          "todo_due",
+                          "user_created_time",
+                          "user_updated_time",
+                        ].indexOf(fieldsArray[field]) > -1
+                      ) {
+                        let dateObject = new Date(
+                          queryNotes.items[queryNotesKey][fieldsArray[field]]
+                        );
+                        if(fieldsArray[field] === "todo_due" && dateObject.getTime() < now.getTime()){
+                          noteInfos.push("<font color='red'>" + dateObject.toLocaleString() + "</font>");
+                        }
+                        else{
+                          noteInfos.push(dateObject.toLocaleString());
+                        }
+                        
+                      } else {
+                        noteInfos.push(
+                          queryNotes.items[queryNotesKey][fieldsArray[field]]
+                        );
+                      }
+                    }
+                    newBody.push("| " + noteInfos.join(" | ") + " |");
+                  }
+                }
+              } while (queryNotes.has_more);
+              let newBodyStr = settingsBlock + "\n" + newBody.join("\n");
+              if (noteBody != newBodyStr) {
+                console.info("Update note " + noteTitle + " (" + noteId + ")");
+                await joplin.data.put(["notes", noteId], null, {
+                  body: newBodyStr,
+                });
+              }
+            } else {
+              console.info("No search query");
+            }
+          }
+        }
+      } while (overviewNotes.has_more);
+
+      window.setTimeout(runCreateNoteOverview, 1000 * 60 * 5);
+    }
+
+    // extract settings from block
+    async function getParameter(
+      settings: string,
+      parameter: string,
+      defval: string
+    ): Promise<string> {
+      var regex = new RegExp("^" + parameter + ":\s?(.*)$", "im");
+      const match = settings.match(regex);
+      if (match) {
+        return match[1].trim();
+      } else {
+        return defval;
+      }
+    }
+
+    runCreateNoteOverview();
+  },
+});
